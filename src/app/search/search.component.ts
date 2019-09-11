2fb0aa00726b968d63a94fa64a33a091a4b8275a
Ismael Milán Márquez <imilanma@everis.com>
2019-09-11 10:11:21 +0200

    taller5

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { OwnerService } from './services/owner.service';
 
+interface Owner {
+  id: number;
+  firstName: string;
+  lastName: string;
+  address: string;
+  city: string;
+  telephone: string;
+  pets : any[];
+}
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -9,6 +19,8 @@ import { OwnerService } from './services/owner.service';
 export class SearchComponent implements OnInit {
   public query: string;
   public result: string;
+  public response: Owner[];
+  public owners: Owner[];
 
   @Input() labelButton: string;
   @Input() placeholder: string;
@@ -16,19 +28,32 @@ export class SearchComponent implements OnInit {
 
   constructor(private owner: OwnerService) {
     this.query = '';
-    this.owner.getOwners();
   }
 
   ngOnInit() {
     if (this.labelButton === '' || !this.labelButton) {
       this.labelButton = 'Buscar!';
     }
+    this.owners = [];
     this.placeholder = this.placeholder ? this.placeholder : '';
+    this.getOwners();
   }
-  search(event) {
+  search(event): void {
     event.preventDefault();
     this.result = `Resultado para la búsqueda de "${this.query}".`;
     this.searchEvent.emit({ query: this.query });
+    this.owners = this.response.filter(owner => {
+      let fields = ['firstName', 'lastName', 'address', 'city'];
+      for (let field of fields) {
+        return (owner[field].indexOf(this.query) != -1);
+      }
+    });
+  }
 
+  getOwners(): void {
+    this.owner.getOwners().subscribe(data => {
+      this.response = JSON.parse(data['_body']);
+      this.owners = JSON.parse(data['_body']);
+    });
   }
 }
