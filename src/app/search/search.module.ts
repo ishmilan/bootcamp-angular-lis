c6256e8e6a40197e12e63d06522493e2119f1ccd
Ismael Milán Márquez <imilanma@everis.com>
2019-09-11 13:09:54 +0200

    primeng

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -5,12 +5,16 @@ import { OwnerService } from './services/owner.service';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { ListOwnersComponent } from './list-owners/list-owners.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     HttpModule,
+    InputTextModule,
+    ButtonModule
   ],
   declarations: [SearchComponent, ListOwnersComponent],
   providers: [OwnerService],
